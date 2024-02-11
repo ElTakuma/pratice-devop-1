7761b7cd39ffbb9e3dd2d76499c4a92b62b26a0b
Roméo Tatchemo <transfergratisromeo@gmail.com>
2024-02-11 01:16:21 +0100

    update home

--- src/main/java/com/devops/praticedevop/web/rest/HomeResource.java
+++ src/main/java/com/devops/praticedevop/web/rest/HomeResource.java
@@ -1,15 +1,20 @@
 package com.devops.praticedevop.web.rest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("")
 public class HomeResource {
 
     @GetMapping
-    public String index() {
-        return "Welcome to you";
+    public String index(Model model) {
+        // Add any necessary data to the model
+        model.addAttribute("message", "Welcome to the new page!");
+
+        // Return the logical view name of the page
+        return "home";
     }
 }
---
+++ src/main/resources/templates/home.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Page</title>
+</head>
+<body>
+<h1>New Page</h1>
+<p>${message}</p> <!-- Accessing data from the model -->
+</body>
+</html>
